ffae85975572191da930d2a0f49216b6fda27609
Renan Pato <renan@companyhero.com>
2024-04-18 15:35:48 -0300

    feature: implent flask

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use a imagem oficial do Python como base
+FROM python:3.11
+
+# Define o diretório de trabalho dentro do contêiner
+WORKDIR /app
+
+# Copia o arquivo requirements.txt para o diretório de trabalho
+COPY requirements.txt .
+
+# Instala as dependências do projeto
+Run pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copia o arquivo main.py para o diretório de trabalho
+COPY main.py .
+
+# Define a porta que o contêiner irá expor
+EXPOSE 5000
+
+# Comando para executar o aplicativo quando o contêiner for iniciado
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:main"]
---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+from flask import Flask, render_template, request, jsonify
+from jinja2 import environment
+from utils.draw import VigaDraw
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/iBeam')
+def iBeam():
+    return render_template('iBeam.html')
+
+@app.route('/tBeam')
+def tBeam():
+    return render_template('tBeam.html')
+
+@app.route('/draw', methods=['POST',])
+def draw():
+    data = json.loads(request.data.decode('utf-8'))
+    print( data["h"], data["d"], data["bw"], data["M"])
+    image = VigaDraw(data["h"], data["d"], data["bw"], data["M"])
+    print(image.CalcArmorDistante(2, 2))
+    image.Plot()
+    image.DrawViga(True, True)
+
+    circles = [((15, 30), .5), ((5, 20), .5), ((25, 40), .5)]
+    image.DrawMultipleArmor(circles)
+    image.CalcArmorDistante(4, 12)
+    graf = image.CalcArmorDistante(2,2)
+    print(graf.show())
+    return request.data
